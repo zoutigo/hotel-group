95c9462baf5cc65e8dc6a23ee585d53d29fda863
zoutigo <zoutigo@gmail.com>
2022-03-25 08:44:42 +0100

    initialize notistack

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { render } from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
+import { SnackbarProvider } from "notistack"
+import { StoreProvider } from "../utils/Store"
 import "./styles/app.css"
 import Home from "./js/components/Home"
 
 render(
-  <Router>
-    <Home />
-  </Router>,
+  <SnackbarProvider anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+    <StoreProvider>
+      <Router>
+        <Home />
+      </Router>
+    </StoreProvider>
+  </SnackbarProvider>,
   document.getElementById("root")
 )
