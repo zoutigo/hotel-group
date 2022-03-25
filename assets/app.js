4e50e09263a2f2de737daff2bb9bc1ed54de0c73
zoutigo <zoutigo@gmail.com>
2022-03-25 09:36:59 +0100

    set queryclient provider

--- assets/app.js
+++ assets/app.js
@@ -2,20 +2,36 @@ import React from "react"
 import { render } from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 import { SnackbarProvider } from "notistack"
+import { QueryClient, QueryClientProvider } from "react-query"
 import { StoreProvider } from "../utils/Store"
 import "./styles/app.css"
 import Home from "./js/components/Home"
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: true,
+      retry: 1,
+      retryDelay: 500,
+    },
+  },
+})
+
 render(
-  <ThemeProvider theme={theme}>
-    <SnackbarProvider anchorOrigin={{ vertical: "top", horizontal: "center" }}>
-      <StoreProvider>
-        <Router>
-          <Home />
-        </Router>
-      </StoreProvider>
-    </SnackbarProvider>
-  </ThemeProvider>,
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <StoreProvider>
+          <Router>
+            <Home />
+          </Router>
+        </StoreProvider>
+      </SnackbarProvider>
+    </ThemeProvider>
+  </QueryClientProvider>,
 
   document.getElementById("root")
 )
