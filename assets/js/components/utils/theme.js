599bdff321b6559feb20a9e42cd07424711503ae
zoutigo <zoutigo@gmail.com>
2022-03-28 06:59:36 +0200

    add colors and polices in the theme

--- assets/js/components/utils/theme.js
+++ assets/js/components/utils/theme.js
@@ -3,23 +3,81 @@ import { createTheme, responsiveFontSizes } from "@material-ui/core"
 const initialtheme = createTheme({
   typography: {
     h1: {
-      fontSize: "1.6rem",
-      fontWeight: 400,
+      fontSize: "3.5rem",
+      lineHeight: "120%",
+      fontWeight: "bold",
       margin: "1rem 0",
     },
     h2: {
-      fontSize: "1.4rem",
-      fontWeight: 400,
+      fontSize: "3rem",
+      lineHeight: "120%",
+      fontWeight: "bold",
+      margin: "1rem 0",
+    },
+    h3: {
+      fontSize: "1.75rem",
+      lineHeight: "150%",
+      fontWeight: "bold",
+      margin: "1rem 0",
+    },
+    h4: {
+      fontSize: "1.25rem",
+      lineHeight: "150%",
+      fontWeight: "bold",
+      margin: "1rem 0",
+    },
+    h5: {
+      fontSize: "0.875rem",
+      lineHeight: "150%",
+      fontWeight: "bold",
+      margin: "1rem 0",
+    },
+    h6: {
+      fontSize: "0.75rem",
+      lineHeight: "150%",
+      fontWeight: "bold",
+      margin: "1rem 0",
+    },
+    body1: {
+      fontSize: "1rem",
+      lineHeight: "160%",
+      fontWeight: "normal",
+      margin: "1rem 0",
+    },
+    body2: {
+      fontSize: "0.875rem",
+      lineHeight: "160%",
+      fontWeight: "normal",
       margin: "1rem 0",
     },
   },
   palette: {
-    type: darkMode ? "dark" : "light",
     primary: {
-      main: "#f0c000",
+      main: "#70C174",
     },
     secondary: {
-      main: "#208080",
+      main: "#BEF3C0",
+    },
+    tertiary: {
+      main: "#EFF7F2",
+    },
+    primarytext: {
+      main: "#1D2130",
+    },
+    secondarytext: {
+      main: "#525560",
+    },
+    offwhite: {
+      main: "#EBF0F9",
+    },
+    border: {
+      main: "#E5E5E5",
+    },
+    black: {
+      main: "#0B0706",
+    },
+    white: {
+      main: "#FFFFFF",
     },
   },
 })
