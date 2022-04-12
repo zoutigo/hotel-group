0b4281e470721e935de99b7029babca2038f3ca7
zoutigo <zoutigo@gmail.com>
2022-04-12 13:23:36 +0200

    🐛 FIX: created at post validate

---
+++ src/EventSubscriber/PostSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use DateTime;
+use App\Entity\House;
+use App\Entity\Image;
+use App\Entity\Booking;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class PostSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['willSetCreatedAt', EventPriorities::POST_VALIDATE],
+        ];
+    }
+
+    public function willSetCreatedAt(ViewEvent $event): void
+    {
+        $object = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+       
+        if (Request::METHOD_POST === $method) {
+            $now = new DateTime('now');
+            $object->setCreatedAt($now);
+        }
+    }
+}
